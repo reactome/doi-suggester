5a702dab9264882c58ed3b290961d072742cdbd7
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2021-05-11 16:47:47 -0400

    Catch FileNotFoundException

--- src/main/java/org/reactome/qa/doisuggester/DOISuggesterMain.java
+++ src/main/java/org/reactome/qa/doisuggester/DOISuggesterMain.java
@@ -2,6 +2,7 @@ package org.reactome.qa.doisuggester;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -34,6 +35,7 @@ public class DOISuggesterMain
 
 	public static void main(String[] args) throws IOException
 	{
+		// TODO: Use log4j instead of printing to the console.
 		System.out.println("Checking for RLEs that might need a DOI...");
 		String pathToConfig = Paths.get("src", "main", "resources", "config.properties").toString();
 		String username;
@@ -64,7 +66,7 @@ public class DOISuggesterMain
 			System.out.println(rleList.size() + " Reaction-like events will be checked.");
 			Map<String, Integer> rleCountsForPathways = new HashMap<>();
 			List<List<String>> records = new ArrayList<>();
-				
+
 			for (GKInstance currentRLE : rleList)
 			{
 				// Ignore inferred RLEs
@@ -90,7 +92,7 @@ public class DOISuggesterMain
 					}
 				}
 			}
-			
+
 			int total = 0;
 			for (Entry<String, Integer> entry : rleCountsForPathways.entrySet() )
 			{
@@ -106,27 +108,33 @@ public class DOISuggesterMain
 			e.printStackTrace();
 			System.exit(-1);
 		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("The config.properties file could not be found. Now exiting.");
+			System.exit(-1);
+		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
+			System.exit(-1);
 		}
 		catch (InvalidAttributeException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(-1);
 		}
 		catch (Exception e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(-1);
 		}
 		System.out.println("All done.");
 	}
 
 	/**
 	 * Print the suggestions to a file.
-	 * @param suggestions
-	 * @throws IOException
+	 * @param suggestions A list of lists - each sublist is an element of a suggestion.
+	 * @throws IOException - Could be thrown if there is a problem writing the output to a file.
 	 */
 	private static void printOutput(List<List<String>> records) throws IOException
 	{
@@ -136,9 +144,9 @@ public class DOISuggesterMain
 		try (CSVPrinter printer = new CSVPrinter(new FileWriter(new File("doi-suggestions.csv")), CSVFormat.DEFAULT.withQuoteMode(QuoteMode.ALL).withHeader("Pathway","RLE", "InstanceEdits")))
 		{
 			// This comparator is used to ensure the outputs are neatly sorted by Pathway, and then by RLE.
-			// 
+			//
 			// Since we're sorting the string representation of these objects, the output will actually end up being sorted by DB ID,
-			// since string representations are usually structured as something like "[${DB_ID}] ${_displayName}". But that's not a big deal - we just 
+			// since string representations are usually structured as something like "[${DB_ID}] ${_displayName}". But that's not a big deal - we just
 			// want *SOME* ordering imposed so records of different Pathways are all grouped together in the output. Otherwise, the report is harder
 			// to read.
 			Comparator<List<String>> recordComparator = (arg0, arg1) -> {
@@ -152,7 +160,7 @@ public class DOISuggesterMain
 					}
 					return compareResult;
 				};
-			
+
 			for (List<String> record : records.stream().sorted(recordComparator).collect(Collectors.toList()))
 			{
 				printer.printRecord(record.get(pathwayIndex), record.get(rleIndex), record.get(instanceEditIndex));
